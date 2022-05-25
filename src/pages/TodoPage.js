1cbb362d793f9157e02d2bfb25d0b5d5d45fdd97
Oleg Zaidullin <cornknight@gmail.com>
2022-05-25 10:50:14 +0600

    list of items example

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,5 +1,16 @@
 function TodoPage() {
-    return <div><h1>TodoPage</h1></div>
+    const todoList = ['Item 1', 'Item 2', 'Item 3']
+
+    return <div>
+        <h1>TodoPage</h1>
+        <ul>
+            {todoList.map((item, idx) => (
+                <li>
+                    {idx+1}. {item}
+                </li>
+            ))}
+        </ul>
+    </div>
 }
 
 export default TodoPage;
