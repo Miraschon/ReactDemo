6a7473c3866cca0d47aa2b05fd9cbbcbb7ddc2b5
Oleg Zaidullin <cornknight@gmail.com>
2022-06-03 12:47:48 +0600

    fixed some linter issues

--- src/pages/ReactStateClass.js
+++ src/pages/ReactStateClass.js
@@ -1,48 +1,49 @@
-import * as React from "react";
-import {Link} from "react-router-dom";
+import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 class ReactStateClass extends React.Component {
-
     constructor(props) {
-        super(props);
+        super(props)
+        const { number } = props
         this.state = {
-            num1: this.props.number,
+            num1: number,
             num2: 2
         }
-        console.log('constructor', this.state.num)
+        console.log('constructor', number)
     }
 
     increase1() {
-        this.setState({ num1: this.state.num1+1 }, () => {
-            console.log('increase num',this.state.num1)
+        const { num1 } = this.state
+        this.setState({ num1: num1 + 1 }, () => {
+            console.log('increase num', num1)
         })
     }
 
-
     increase2() {
-        this.setState({ num2: this.state.num2+1 }, () => {
-            console.log('increase num',this.state.num2)
+        this.setState({ num2: this.state.num2 + 1 }, () => {
+            console.log('increase num', this.state.num2)
         })
     }
 
     render() {
         console.log('render num', this.state.num1)
-        return <div>
-            <button className={'table'}><Link to="/">Table of contents</Link></button>
-            <div className="wrap">
-                <h1>React State Class</h1>
-                <p>{this.state.num1}</p>
-                <p>{this.state.num2}</p>
-                {/*
+        return (
+          <div>
+                <button className="table"><Link to="/">Table of contents</Link></button>
+                <div className="wrap">
+                    <h1>React State Class</h1>
+                    <p>{this.state.num1}</p>
+                    <p>{this.state.num2}</p>
+                    {/*
                 The method this.increase loses external context (this) if used as an argument
                 That's why we should explicitly point the method to its context.
                 */}
-                <button onClick={this.increase1.bind(this)}>Increase num1</button>
-                <button onClick={this.increase2.bind(this)}>Increase num2</button>
+                    <button onClick={this.increase1.bind(this)}>Increase num1</button>
+                    <button onClick={this.increase2.bind(this)}>Increase num2</button>
             </div>
-        </div>
+            </div>
+        )
     }
-
 }
 
 export default ReactStateClass
